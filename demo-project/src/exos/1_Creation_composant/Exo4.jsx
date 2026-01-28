298891c917470160674f70a09ee9aae7928efd8f
Denisa Furtuna <d.furtuna@interface3.be>
2026-01-28 10:14:00 +0100

    correction eco4

--- demo-project/src/exos/1_Creation_composant/Exo4.jsx
+++ demo-project/src/exos/1_Creation_composant/Exo4.jsx
@@ -1,4 +1,7 @@
+import styles from './Exo4.module.css';
+
 import { useState } from "react";
+ 
  
 export const Exo4 = () => {
  
@@ -7,37 +10,43 @@ export const Exo4 = () => {
             id: 1,
             name: 'Burger Cheese',
             price: 8.50,
-            quantity: 0
+            quantity: 0,
+            image:"https://images.unsplash.com/photo-1692688649681-901967b9d50c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
         },
         {
             id: 2,
             name: 'Burger Chicken',
             price: 11,
-            quantity: 0
+            quantity: 0,
+            image:"https://plus.unsplash.com/premium_photo-1695758787947-0aff87c1f93a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             id: 3,
             name: 'Burger Triple',
             price: 12.50,
-            quantity: 0
+            quantity: 0,
+            image:"https://images.unsplash.com/photo-1552718752-c682d315b136?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyJTIwdHJpcGxlfGVufDB8fDB8fHww"
         },
         {
             id: 4,
             name: 'Petite Frite',
             price: 3.50,
-            quantity: 0
+            quantity: 0,
+            image:"https://media.istockphoto.com/id/1312992473/photo/tasty-delicious-and-fresh-fried-french-potatoes-in-basket-over-wooden-table.webp?a=1&b=1&s=612x612&w=0&k=20&c=CYie6Z0-QPVxHGUjdXHn9bzrC0vIl8LKGHjRldCAVcA="
         },
         {
             id: 5,
             name: 'Moyenne Frite',
             price: 4.50,
-            quantity: 0
+            quantity: 0,
+            image:"https://media.istockphoto.com/id/1312992473/photo/tasty-delicious-and-fresh-fried-french-potatoes-in-basket-over-wooden-table.webp?a=1&b=1&s=612x612&w=0&k=20&c=CYie6Z0-QPVxHGUjdXHn9bzrC0vIl8LKGHjRldCAVcA="
         },
         {
             id: 6,
             name: 'Grande Frite',
             price: 6,
-            quantity: 0
+            quantity: 0,
+            image:"https://media.istockphoto.com/id/1312992473/photo/tasty-delicious-and-fresh-fried-french-potatoes-in-basket-over-wooden-table.webp?a=1&b=1&s=612x612&w=0&k=20&c=CYie6Z0-QPVxHGUjdXHn9bzrC0vIl8LKGHjRldCAVcA="
         },
     ]);
  
@@ -72,36 +81,36 @@ export const Exo4 = () => {
  
         setTotal(somme);
  
-        //todo
-        // pas ici mais créer un state pour le total
-        // Opt 1 :
-        // récupérer le produit grâce à son id
-        // modifier ton total avec le set pour qu'il soit égal à lui même - ou + le prix du produit trouvé juste au dessus
-        // prix * value
- 
-        //Opt 2 :
-        //calculer la somme avec une boucle ou un reduce
     }
  
     return (
-        <div>
+        <div className={styles.h2}>
             <h2>Gestion de panier :</h2>
             <div>
                 {products.map(product => (
                     <div key={product.id} className="flex flex-row items-center gap-3" >
+
+<img 
+    src={product.image} 
+    alt={product.name}
+    className={styles.productImage}  
+
+/>
+                 
  
-                        <p>{product.name}</p> <p>{product.price}</p>
+                        <p className={styles.name}>{product.name}</p> <p className={styles.name}>{product.price.toFixed(2)}€</p>
+                        
  
-                        <div className="flex flex-row justify-center items-center gap-3">
-                            <button onClick={() => crement(product.id, -1)} >➖</button>
+                        <div className={styles.buttons}>
+                            <button  disabled={product.quantity === 0}  onClick={() => crement(product.id, -1)} >➖</button>
                             <p>{product.quantity}</p>
                             <button onClick={() => crement(product.id, 1)} >➕</button>
                         </div>
  
                     </div>))}
             </div>
-            <div>
-                Total : {total} €
+            <div className={styles.total}>
+                Coût total : {total} €
             </div>
         </div>
     )
