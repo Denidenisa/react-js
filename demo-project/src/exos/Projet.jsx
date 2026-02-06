f6dbbde8524a94a1c6952fcd3317df31a0610504
Denisa Furtuna <d.furtuna@interface3.be>
2026-02-06 10:41:37 +0100

    ajout demo aude

---
+++ demo-project/src/exos/Projet.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import style from './Projet.module.css';
+import { Compteur } from './Compteur';
+
+export const Projet = () => {
+    // useState -> Créer des variables qui vont être amenées à être modifiées
+    const [compteurs, setCompteurs] = useState([
+        {
+            id: 1,
+            name: 'Mailles',
+            isVisible: true
+        },
+        {
+            id: 2,
+            name: 'Rangs',
+            isVisible: true
+        }
+    ])
+
+    const changeVisibility = (id) => {
+        const newCompteurs = compteurs.map(compteur => { 
+            if(compteur.id === id) {
+                compteur.isVisible = !compteur.isVisible
+            }
+            return compteur;
+         });
+
+         setCompteurs(newCompteurs);
+    }
+
+    return (
+        <div className={style.container}>
+
+            <div className={style.projet}>
+                <h2>Projet de tricot n°1548695</h2>
+
+                {
+                    compteurs.map(compteur => (
+                        <div key={compteur.id} className={style.group}>
+                            <h3 className={style.title}>{compteur.name}</h3>
+
+                            {compteur.isVisible && <Compteur compteur={compteur} />}
+
+                            <button
+                                onClick={() => { changeVisibility(compteur.id) }}>
+                                {compteur.isVisible ?
+                                    '🙈' : '👀'}
+                            </button>
+                        </div>
+                    ))
+                }
+
+
+            </div>
+
+        </div>
+    )
+}
